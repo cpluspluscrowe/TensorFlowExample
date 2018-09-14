2d573aaab4f489af078cbb72e32717822182b4b6
Chad Crowe <ccrowe@linkedin.com>
2018-09-13 20:26:52 -0500

    got tensorboard working

--- NeuralNetworkExample.py
+++ NeuralNetworkExample.py
@@ -1,45 +1,50 @@
-from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import datetime
+from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-learning_rate = 0.5
-epochs = 10
-batch_size = 100
+x = tf.placeholder(tf.float32, shape=[None, 784])
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
+W = tf.get_variable("weights1", shape=[784, 10],
+                    initializer=tf.glorot_uniform_initializer())
 
-W1 = tf.Variable(tf.random_normal([784, 300], stddev=0.03), name='W1')
-b1 = tf.Variable(tf.random_normal([300]), name='b1')
+b = tf.get_variable("bias1", shape=[10],
+                    initializer=tf.constant_initializer(0.1))
 
-W2 = tf.Variable(tf.random_normal([300, 10], stddev=0.03), name='W2')
-b2 = tf.Variable(tf.random_normal([10]), name='b2')
+y = tf.nn.relu(tf.matmul(x, W) + b)
 
-hidden_out = tf.add(tf.matmul(x, W1), b1)
-hidden_out = tf.nn.relu(hidden_out)
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W2), b2))
-y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
-
-cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
-                                              + (1 - y) * tf.log(1 - y_clipped), axis=1))
-
-optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
-
-init_op = tf.global_variables_initializer()
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_)
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-with tf.Session() as sess:
-    sess.run(init_op)
-    total_batch = int(len(mnist.train.labels) / batch_size)
-    for epoch in range(epochs):
-        avg_cost = 0
-        for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size)
-            _, c = sess.run([optimiser, cross_entropy], feed_dict={x: batch_x, y: batch_y})
-            avg_cost += c / total_batch
-        print("Epoch: ", (epoch + 1), "cost = ", "{:.3f}".format(avg_cost))
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+sess = tf.InteractiveSession()
+
+tf.summary.scalar('accuracy', accuracy)
+
+time_string = datetime.datetime.now().isoformat()
+experiment_name = f"one_hidden_layer_1000_steps_{time_string}"
+
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter(f'./train/{experiment_name}', sess.graph)
+test_writer = tf.summary.FileWriter(f'./test/{experiment_name}', sess.graph)
+
+tf.global_variables_initializer().run()
+
+for step in range(1000):
+    print(f"training step: {step}")
+    if step % 10 == 0:
+        summary, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images,
+                                                               y_: mnist.test.labels})
+        test_writer.add_summary(summary, step)
+        print(f'Step {step}; Model accuracy: {acc}')
+    else:
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+        summary = sess.run(merged, feed_dict={x: batch_xs,
+                                              y_: batch_ys})
+        train_writer.add_summary(summary, step)
